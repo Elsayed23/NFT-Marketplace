353dd8bc1cd01494832a795b007cfa7d886ad1f2
elsayedkewan <107002426+Elsayed23@users.noreply.github.com>
2023-05-25 19:36:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ buttonSh.addEventListener("click", () => {
 remove.addEventListener("click", () => {
     show.classList.remove("show-nav")
 })
+
+let header = document.querySelector("header");
+let fixedCon = document.querySelector("header .container");
+
+
+window.addEventListener("scroll", () => {
+    if (this.scrollY >= 500) {
+        header.classList.add("fixedHeader");
+        fixedCon.classList.add("fixedCon");
+    } else {
+        header.classList.remove("fixedHeader");
+        fixedCon.classList.remove("fixedCon");
+    }
+})
